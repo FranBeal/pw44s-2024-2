d880da16028fd5e95184a2c3e9b9bf0f08f80d76
FranBeal <francielebeal@gmail.com>
2024-10-14 09:20:46 -0300

    Aula 11/10/2024

---
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/User.java
@@ -0,0 +1,67 @@
+package br.edu.utfpr.pb.pw44s.server.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name="user_tb")
+@Getter @Setter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 4, max = 255)  // valida para que o atributo tenha entre 4 e 255 caracteres
+    private String username;
+
+    @NotNull // valida para que o atributo não seja nulo
+    private String displayName;
+
+    @NotNull
+    @Size(min = 6, max = 254)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    private String password;
+    //valida para que o atributo tenha pelo menos 1 letra maiúscula,
+    // 1 letra minúscula e 1 número.
+
+    //Metodo que irá retornar a lista de permissões
+    @Override
+    @Transient
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.createAuthorityList("Role_USER");
+    }
+
+    @Override
+    @Transient
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient  public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @Transient  public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @Transient  public boolean isEnabled() {
+        return true;
+    }
+}
