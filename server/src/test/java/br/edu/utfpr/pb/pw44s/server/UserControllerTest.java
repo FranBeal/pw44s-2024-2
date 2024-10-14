732e64ef556cb6558bba87e9b2ee8cb8d18b94e8
FranBeal <francielebeal@gmail.com>
2024-10-14 09:20:46 -0300

    Aula 11/10/2024

---
+++ server/src/test/java/br/edu/utfpr/pb/pw44s/server/UserControllerTest.java
@@ -0,0 +1,100 @@
+package br.edu.utfpr.pb.pw44s.server;
+
+import br.edu.utfpr.pb.pw44s.server.model.User;
+import br.edu.utfpr.pb.pw44s.server.repository.UserRepository;
+import br.edu.utfpr.pb.pw44s.server.shared.GenericResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class UserControllerTest {
+    private static final String API_USERS = "/users";
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    public void cleanup() {
+        userRepository.deleteAll();
+        testRestTemplate.getRestTemplate().getInterceptors().clear();
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_receiveOk() {
+        User user = new User();
+        user.setUsername("test-user");
+        user.setDisplayName("test-Display");
+        user.setPassword("P4ssword");
+
+        ResponseEntity<Object> response = testRestTemplate.postForEntity("/users", user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_userSavedToDatabase() {
+        User user = createValidUser();
+        testRestTemplate.postForEntity("/users", user, Object.class);
+        // Agora precisamos garantir que tudo foi salvo no Banco de Dados.
+        assertThat(userRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_receiveSuccessMessage() {
+        User user = createValidUser();
+        ResponseEntity<GenericResponse> response = testRestTemplate.postForEntity(API_USERS, user, GenericResponse.class);
+        assertThat(response.getBody().getMessage()).isNotNull();
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_passwordIsHashedInDatabase() {
+        User user = createValidUser();
+        testRestTemplate.postForEntity(API_USERS, user, Object.class);
+        List<User> users = userRepository.findAll();
+        User userBD = users.get(0);
+        assertThat(userBD.getPassword()).isNotEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void postUser_whenUserHasNullUsername_receiveBadRequest() {
+        User user = createValidUser();
+        user.setUsername(null);
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void postUser_whenUserHasNullPassword_receiveBadRequest() {
+        User user = createValidUser();
+        user.setPassword(null);
+        ResponseEntity<Object> response = postSignup(user, Object.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+
+    private User createValidUser(){
+        User user = new User();
+        user.setUsername("test-user");
+        user.setDisplayName("test-Display");
+        user.setPassword("P4ssword");
+        return user;
+    }
+
+    public <T> ResponseEntity<T> postSignup(Object request,  Class<T> responseType) {
+        return testRestTemplate.postForEntity(API_USERS, request, responseType);
+    }
+
+}
